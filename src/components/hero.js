b39935b2d979005eade3b8fab785c114664bffe6
obirije <obirijejohn@gmail.com>
2018-11-22 17:05:32 +0100

    conversion calculator implementation and API and build deployment

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,132 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
+import axios from 'axios';
+
+const fiatCurr = [
+  { value: 'NGN', label: 'NGN' },
+  { value: 'USD', label: 'USD' }
+];
+
+const cryptoCurr = [
+  { value: 'BTC', label: 'BTC' },
+  { value: 'ETH', label: 'ETH' }
+];
 
 export default class Hero extends Component {
+
+    state = {
+        fiatSelectedOption: { value: 'NGN', label: 'NGN' },
+        cryptoSelectedOption: { value: 'BTC', label: 'BTC' },
+        fiatAmount: '0.00',
+        cryptoAmount: '0.00',
+        BTC_USD: null,
+        ETH_USD: null,
+        ngnRate: 360
+      }
+
+      handleFiatChange = (selectedOption) => {
+        this.setState({
+            fiatSelectedOption: selectedOption
+        });
+        console.log(`Option selected:`, selectedOption);
+      }
+
+      handleCryptoChange = (selectedOption) => {
+        this.setState({ 
+            cryptoSelectedOption: selectedOption 
+        });
+        console.log(`Option selected:`, selectedOption);
+      }
+
+      handleAmountChange = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+
+        this.setState({
+          [name]: value
+        });
+
+        let price = this.getRates(this.state.fiatSelectedOption.value, this.state.cryptoSelectedOption.value, value, this.state.ngnRate);
+
+        console.log(`input entered: ${value}, price: ${price}`);
+
+        let updateCrypto = name === 'fiatAmount' ? 'cryptoAmount' : 'fiatAmount';
+
+        this.setState({
+            [updateCrypto]: price
+        })
+      }
+
+      getRates = (fromCurr, toCurr, amount, ngn) => { 
+        if(fromCurr === 'USD' ){
+
+            if(toCurr === 'BTC'){
+                const rate = this.state.BTC_USD ? this.state.BTC_USD : 0;
+                return amount * rate;
+            }
+
+            if(toCurr === 'ETH'){
+                const rate = this.state.ETH_USD ? this.state.ETH_USD : 0;
+                return amount * rate;
+            }
+        }
+
+        if(fromCurr === 'NGN'){
+
+            if(toCurr === 'BTC'){
+                const rate = this.state.BTC_USD ? this.state.BTC_USD : 0;
+                return amount * (rate / ngn);
+            }
+
+            if(toCurr === 'ETH'){
+                const rate = this.state.ETH_USD ? this.state.ETH_USD : 0;
+                return amount * (rate / ngn);
+            }
+        }
+      }
+
+      getExchangeRateExternal = (crypto) => {
+        //const NGN = 360;
+        const btc_usd_url = 'https://api.cryptonator.com/api/ticker/btc-usd';
+        const eth_usd_url = 'https://api.cryptonator.com/api/ticker/eth-usd';
+
+        const url =  crypto === 'BTC' ? btc_usd_url : eth_usd_url
+
+        axios.get(url).then( res => {
+
+            if(!res.data.success){
+
+                console.log("error from api getting btc_usd ", res.data);
+                return
+            }
+
+            console.log(`PRICE - ${crypto}`, res.data.ticker.price);
+
+            const price = Number(res.data.ticker.price);
+            const timestamp = res.data.ticker.timestamp;
+
+            // const btc_ngn = btc_usd * NGN;
+
+            const curr_pair = crypto === 'BTC' ? 'BTC_USD' : 'ETH_USD'; 
+
+            this.setState({
+                [curr_pair]: price,
+            });
+
+            //this.getAssetsRates(selectedCryptoy, xlm_usd_url, NGN);
+            
+        }).catch( err => {
+            console.log(`error getting btc rate from ${url}`, err);
+        });
+
+      }
+
+      componentDidMount(){
+        this.getExchangeRateExternal('BTC')
+        this.getExchangeRateExternal('ETH')
+
+      }
+
   render() {
     return (
         <div class="container">
@@ -14,48 +140,53 @@ export default class Hero extends Component {
                     </div>
                     <img src="static/assets/images/cc.png" width="150" alt="logos" />
                 </div>
-                <div class="col l6 right-align p-l-40 m-t-40">
+                <div class="col l6 s12 m8 right-align p-l-40 m-t-40">
                     <div class="card converter-card-radius">
-                            <div class="card-content">
-                                <h5 class="card-title left m-b-30">Currency Converter</h5>
-                                <form>
+                            <div class="card-content white-gradient">
                                     <div class="row">
                                         <div class="input-field">
-                                            <select class="browser-default">
-                                                <option value="" disabled selected>Select currency</option>
-                                                <option value="1">USD</option>
-                                                <option value="2">NGN</option>
-                                            </select>
+                                            
+                                            <Select
+                                                value={this.state.fiatSelectedOption}
+                                                onChange={this.handleFiatChange}
+                                                options={fiatCurr}
+                                              />
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="input-field col s12">
-                                            <input id="email3" type="email" />
-                                            <label for="email3">Amount</label>
+                                            <input id="fiatAmount" 
+                                                type="number" name="fiatAmount"
+                                                value={this.state.fiatAmount} 
+                                                onChange={this.handleAmountChange} />
+                                            <label class="active" for="fiatAmount">Amount</label>
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="input-field">
-                                            <select class="browser-default">
-                                                <option value="" disabled selected>Select currency</option>
-                                                <option value="1">BTC</option>
-                                                <option value="2">ETH</option>
-                                            </select>
+                                            <Select
+                                                value={this.state.cryptoSelectedOption}
+                                                onChange={this.handleCryptoChange}
+                                                options={cryptoCurr}
+                                              />
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="input-field col s12">
-                                            <input id="email3" type="email" />
-                                            <label for="email3">Amount</label>
+                                            <input id="cryptoAmount" 
+                                                type="number"
+                                                name="cryptoAmount"
+                                                value={this.state.cryptoAmount}
+                                                onChange={this.handleAmountChange} />
+                                            <label class="active" for="cryptoAmount">Amount</label>
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="input-field col s12">
-                                            <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Continue
+                                            <button class="btn cyan waves-effect waves-light right" name="action">Continue
                                             </button>
                                         </div>
                                     </div>
-                                </form>
                             </div>
                         </div>
                 </div>
